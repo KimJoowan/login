d41e036f1de02e82ed5b55ac38a48d31ce1632b2
KimJoowan <115052001+KimJoowan@users.noreply.github.com>
2026-08-24 16:24:47 +0900

    Update V3__create_account_lock_function.sql

--- test01/src/main/resources/db/migration/V3__create_account_lock_function.sql
+++ test01/src/main/resources/db/migration/V3__create_account_lock_function.sql
@@ -1,22 +1,34 @@
+-- FUNCTION: public.fn_manage_account_lock()
 
--- Table: public.account_lock
+-- DROP FUNCTION IF EXISTS public.fn_manage_account_lock();
 
--- DROP TABLE IF EXISTS public.account_lock;
+CREATE OR REPLACE FUNCTION public.fn_manage_account_lock()
+    RETURNS trigger
+    LANGUAGE 'plpgsql'
+    COST 100
+    VOLATILE NOT LEAKPROOF
+AS $BODY$
 
-CREATE TABLE IF NOT EXISTS public.account_lock
-(
-    "number" bigint NOT NULL,
-    fail_count bigint NOT NULL DEFAULT 0,
-    active boolean,
-    locked_until timestamp without time zone,
-    CONSTRAINT account_lock_pkey PRIMARY KEY ("number"),
-    CONSTRAINT fk_account_lock_member FOREIGN KEY ("number")
-        REFERENCES public.member ("number") MATCH SIMPLE
-        ON UPDATE NO ACTION
-        ON DELETE CASCADE
-)
+BEGIN
+    -- 1. 회원가입 시 (INSERT): 초기 계정 잠금 정보 등록
+    IF (TG_OP = 'INSERT') THEN
+        INSERT INTO public.account_lock ("number", fail_count, locked_until, active)
+        VALUES (NEW."number", 0, NULL, true);
+        RETURN NEW;
 
-TABLESPACE pg_default;
+    -- 2. 회원정보 수정 시 (UPDATE): 회원 탈퇴 처리 시 계정 비활성화
+    ELSIF (TG_OP = 'UPDATE') THEN
+        IF (OLD.role != 'WITHDRAWN' AND NEW.role = 'WITHDRAWN') THEN
+            UPDATE public.account_lock
+            SET active = false -- true에서 false로 수정
+            WHERE "number" = NEW."number";
+        END IF;
+        RETURN NEW;
+    END IF;
 
-ALTER TABLE IF EXISTS public.account_lock
-    OWNER to postgres;
+    RETURN NULL;
+END;
+$BODY$;
+
+ALTER FUNCTION public.fn_manage_account_lock()
+    OWNER TO postgres;
